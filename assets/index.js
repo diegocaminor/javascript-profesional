35d3b1955b936119823268713dab067ec14d10a1
Diego Camino <diego.camino.r@gmail.com>
2020-02-09 00:58:09 -0500

    Scope, closures, call-apply-bind, prototipo, herencia prototipal

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,12 @@ import MediaPlayer from './media-player.js'
 import AutoPlay from './plugins/autoplay.js'
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay] });
+const player = new MediaPlayer({
+    el: video,
+    plugins: [
+        new AutoPlay
+    ]
+});
 
 const buttonPlay = document.querySelector('#button-play');
 const buttonMute = document.querySelector('#button-mute');
