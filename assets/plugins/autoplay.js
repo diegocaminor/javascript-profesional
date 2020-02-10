0552fbef4c2be5e7dc32ec5d891eefefe00a4b7c
Diego Camino <diego.camino.r@gmail.com>
2020-02-09 19:07:20 -0500

    Getters y setters

--- assets/plugins/autoplay.js
+++ assets/plugins/autoplay.js
@@ -1,10 +1,10 @@
-function AutoPlay() {
-
-}
+function AutoPlay() {}
 
 AutoPlay.prototype.run = function(player) {
-    player.mute();
-    player.play();
-}
+  if (!player.muted) {
+    player.muted = true;
+  }
+  player.play();
+};
 
 export default AutoPlay;
