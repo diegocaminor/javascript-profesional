9c170d9705f342ab08090464b890841b7999c3cb
Diego Camino <diego.camino.r@gmail.com>
2020-02-13 00:04:02 -0500

    Tipos básicos typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,60 @@
-console.log("Hola typescript!!!");
+// Tipos básicos typescript
+// Tipos:
 
-function add(a: number, b: number) {
-    return a + b
+// boolean. Valor verdadero o falso.
+// number. Números.
+// string. Cadenas de texto.
+// string[]. Arreglo del tipo cadena de texto.
+// Array. Arreglo multi-tipo, acepta cadenas de texto o números.
+// enum. Es un tipo especial llamado enumeración.
+// any. Cualquier tipo.
+// object. Del tipo objeto.
+
+// Boolean
+let muted: boolean = true
+muted = false
+
+// Números
+let numerador:number = 42
+let denominador:number = 6
+let resultado = numerador/denominador
+
+// String
+let nombre: string = 'Richard'
+let saludo = `Me llamo ${nombre}`
+
+// Arreglos
+let people: string[] = []
+people = ['Isabel', 'Nicole', 'Diego']
+//people.push("Eduardo");
+
+let peopleAndNumber: Array<string | number> = []
+peopleAndNumber.push('Diego')
+peopleAndNumber.push(2020)
+
+// Enum
+enum Color {
+    Rojo,
+    Verde,
+    Azul,
 }
 
-const sum = add(2, 3)
+let colorFavorito: Color = Color.Azul
+console.log(`Mi color favorito es: ${colorFavorito}`) // Mi color favorito es: 2
+
+// Enum
+enum Color2 {
+    Rojo = 'Rojo',
+    Verde = 'Verde',
+    Azul = 'Azul',
+}
+
+let colorFavorito2: Color2 = Color2.Azul
+console.log(`Mi color favorito es: ${colorFavorito2}`) // Mi color favorito es: Azul
+
+// Any -> cuando no tenemos la certeza de que tipo de variable será una variable, entonces podemos usar la notación :any
+let comodin: any = "Joker"
+comodin = { type: 'Wildcard'}
+
+// Object
+let someObject: object = { type: 'Wildcard'}
